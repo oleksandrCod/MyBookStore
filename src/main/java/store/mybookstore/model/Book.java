5a66777298842eccefb1576f5ffec59207e1fada
OleksandrCod <karpo99old@gmail.com>
2023-09-27 11:56:19 +0300

    Init book

---
+++ src/main/java/store/mybookstore/model/Book.java
@@ -0,0 +1,29 @@
+package store.mybookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "titles", nullable = false)
+    private String title;
+    @Column(name = "authors", nullable = false)
+    private String author;
+    @Column(name = "isbn", nullable = false, unique = true)
+    private String isbn;
+    @Column(name = "prices", nullable = false)
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
