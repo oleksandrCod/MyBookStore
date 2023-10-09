1ee90c7daf5cbcefdc44ae7a160e5499bb936c7a
OleksandrCod <karpo99old@gmail.com>
2023-10-09 08:08:44 +0300

    Del unused import

--- src/main/java/store/mybookstore/dto/book/CreateBookRequestDto.java
+++ src/main/java/store/mybookstore/dto/book/CreateBookRequestDto.java
@@ -3,7 +3,6 @@ package store.mybookstore.dto.book;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Set;
 import lombok.Data;
