08465368d36573d12e0e41f34f33ec08000386e5
OleksandrCod <karpo99old@gmail.com>
2023-10-09 08:05:55 +0300

    Use NotBlank instead of NotNull

--- src/main/java/store/mybookstore/dto/book/CreateBookRequestDto.java
+++ src/main/java/store/mybookstore/dto/book/CreateBookRequestDto.java
@@ -2,6 +2,7 @@ package store.mybookstore.dto.book;
 
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Set;
@@ -9,13 +10,13 @@ import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull(message = "Field title can't be empty!")
+    @NotBlank(message = "Field title can't be empty!")
     private String title;
-    @NotNull(message = "Field author can't be empty!")
+    @NotBlank(message = "Field author can't be empty!")
     private String author;
-    @NotNull(message = "Field isbn can't be empty!")
+    @NotBlank(message = "Field isbn can't be empty!")
     private String isbn;
-    @NotNull(message = "Field price can't be empty")
+    @NotBlank(message = "Field price can't be empty")
     @Min(value = 0, message = "Price can't be less than zero.")
     @Digits(integer = 3, fraction = 3, message = "Invalid value"
             + " for field price. Max number is 3 digits.")
